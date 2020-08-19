12acf1a24ec97b7316bb2b49aa510e0238073540
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-08-19 19:57:00 -0400

    agregar ruta base de la api

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="api-base-url" content="{{ url('/api') }}" />
         <title>Lirmi Test | @yield('title')</title>
 
         <!-- Fonts -->
