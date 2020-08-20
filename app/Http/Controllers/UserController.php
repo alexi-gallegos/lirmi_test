78d35799ab0f6237224c8b7d696d5565e44e6817
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-08-20 11:31:27 -0400

    metodo para mostrar vista usuarios local

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,22 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\User;
 
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    function add_user(Request $request){
-        $user = new User;
-        $user->create_user($request);
-        if(!$user->save()){
-            return response()->json([
-                'message' => 'Usuario no pudo ser creado.'
-            ], 409);
-        }
-        return response()->json([
-            'message' => 'Usuario creado.'
-        ], 201);
+    function index(){
+        return view('usuarios_local');
     }
 }
