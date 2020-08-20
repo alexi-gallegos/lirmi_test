663b1a3bdf1506b1dd9e282ec766ffa5c0c22a74
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-08-19 21:31:02 -0400

    agregar componente de alerta que se muestra al crear usuario

---
+++ resources/js/components/AlertModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-if="showAlert" :class="[baseClass, userCreated ? successClass : errorClass]" role="alert">
+    <strong>{{ message }}</strong>
+    <button type="button" class="close" @click="hideAlert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlertModal",
+  props: {
+    userCreated: {
+      type: Boolean,
+      required: true,
+    },
+    showAlert: {
+        type: Boolean,
+        required: true
+    },  
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      baseClass: "alert alert-dismissible fade show my-3",
+      successClass: "alert-success",
+      errorClass: "alert-danger",
+    }
+  },
+  methods: {
+      hideAlert(){
+          this.$parent.hideAlert();
+      }
+  }
+};
+</script>
+
+<style>
+</style>
