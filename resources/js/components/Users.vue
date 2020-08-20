fc9522f79ab241f60a4063775d31e1f91f76909b
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-08-20 16:08:53 -0400

    agregar funcionalidad para mostrar mensajes de error y presentar componentes

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="col-md-4 mx-auto text-center mt-5">
-    <search-box-user></search-box-user>
-    <ul class="list-group">
-      <users-list v-for="(user,index) in users" :key="index" :user="user"></users-list>
+    <users-not-found-message 
+      v-if="users.length == 0 && fetchingUsers == false"
+    ></users-not-found-message>
+    <p v-if="serverError">Lo sentimos, intente más tarde</p>
+    <search-box-user v-if="useLocalApi == false"></search-box-user>
+    <ul v-if="users.length > 0" class="list-group">
+      <users-list
+        v-for="(user,index) in users"
+        :key="index"
+        :user="user"
+        :use-local-api="useLocalApi"
+      ></users-list>
     </ul>
-    <a 
-      v-if="useLocalApi == false" 
-      :href="url"
-    >Ir a usuarios local</a>
+    <a v-if="useLocalApi == false" :href="url">Ir a usuarios local</a>
     <paginate
-      v-if="users.length > 0"
+      v-if="users.length > 0 && pageCount > 1"
       :page-count="pageCount"
       :click-handler="paginationHandler"
       :prev-text="'Prev'"
@@ -28,12 +34,16 @@
 import UsersList from "./UsersList";
 import ModalUser from "./ModalUser";
 import SearchBoxUser from "./SearchBoxUser";
+import { reloadList } from "../eventBus";
+import Swal from "sweetalert2";
+import UsersNotFoundMessage from "./UsersNotFoundMessage";
 export default {
   name: "Users",
   components: {
     UsersList,
     ModalUser,
     SearchBoxUser,
+    UsersNotFoundMessage,
   },
   props: {
     useLocalApi: {
@@ -48,25 +58,42 @@ export default {
     return {
       users: [],
       pageCount: 0,
+      deletedUser: false,
+      fetchingUsers: true,
+      serverError: false
     };
   },
   methods: {
     getUsers(page = 1) {
       let axiosApi = this.useLocalApi ? window.localApi : window.externalApi;
+      this.fetchingUsers = true;
+      this.serverError = false;
       axiosApi
         .get(`/users?page=${page}`)
         .then((res) => {
-          console.log(res);
           this.users = res.data.data;
-          this.pageCount = res.data.total_pages;
+          if (res.data.total_pages) {
+            this.pageCount = res.data.total_pages;
+          } else {
+            this.pageCount = res.data.last_page;
+          }
         })
-        .catch((err) => console.log(err));
+        .catch((err) => this.serverError = true)
+        .finally(() => this.fetchingUsers = false);
     },
     paginationHandler(pageNum) {
       this.getUsers(pageNum);
     },
   },
   mounted() {
+    reloadList.$on("reloadTable", () => {
+      Swal.fire({
+        title: "Eliminado.",
+        text: "Usuario eliminado correctamente.",
+        icon: "success",
+      });
+      this.getUsers();
+    });
     this.getUsers();
   },
 };
