928519773bfec0ee95e3559e0fc714aea043ee05
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-08-19 23:36:13 -0400

    agregar componente paginación

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-
+var Paginate = require('vuejs-paginate');
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -20,6 +20,8 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('users', require('./components/Users.vue').default);
+Vue.component('paginate', Paginate);
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
